855ba2fd5c37568760e77d1dde4e622945c93d2a
kosta-bizetic <kosta.bizetic@gmail.com>
2019-06-30 10:39:04 +0200

    Add project files.

---
+++ iep-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iep_project.Startup))]
+namespace iep_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
