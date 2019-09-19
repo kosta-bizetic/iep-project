3f2b7fcc432d7ac2a1fa1ca757dae826b26d5e5f
kosta-bizetic <kosta.bizetic@gmail.com>
2019-09-20 01:07:06 +0200

    Fine tuning

--- iep-project/Controllers/HomeController.cs
+++ iep-project/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace iep_project.Controllers
 {
@@ -24,7 +20,7 @@ namespace iep_project.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact";
 
             return View();
         }
